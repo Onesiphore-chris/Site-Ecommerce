1a5636c83b23dc275b3398c574e8d3dbfaa92ca2
Onesiphore-chris <chrisleconquerant01@gmail.com>
2022-07-18 15:07:18 +0000

    first commit

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+try {
+    $dbb = new PDO('mysql:host=localhost;dbname=CRUD; charset=utf8', 'root' , '');
+}
+ catch ( Exception $e) 
+ {
+    die( 'kjkjkjkj' .$e -> getmessage());
+}
+
+?>
